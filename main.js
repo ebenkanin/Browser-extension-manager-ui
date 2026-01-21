72a0e9536b832dd5a6a019b2bc5a7c4cb867f237
ebenkanin <ebenezerkanin@gmail.com>
2026-01-21 12:42:46 +0900

    fixing the render logic to look up filter state first

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// grabbing DOM items
+
 const cardContainer = document.querySelector('.cards-container');
 
 const template = document.getElementById('card-template');
@@ -10,16 +12,18 @@ const showAll = document.getElementById('show-all')
 
 const filterInactive = document.getElementById('filter-inactive')
 
-let currentFilter = 'All'
+const filter = document.querySelectorAll('[data-filter]')
 
-filterActive.getAttribute('data-filter')
+let currentFilter = 'all'
+
+
 
 
 
 //
 
 
-//on page load, it retrieves data from the json file, store it in the local storage and render
+//on page load, it retrieves data from the json file, stores it in the local storage and render
 
 window.addEventListener('DOMContentLoaded', innit)
 
@@ -37,6 +41,14 @@ function sendDataToLocalStorage(item){
         dataFromLocalStorage= JSON.parse(localStorage.getItem('items'))
     }
 
+    if(!item.id){
+
+        item.id = crypto.randomUUID()
+
+    }
+
+    
+
     dataFromLocalStorage.push(item)
 
     
@@ -49,74 +61,27 @@ function sendDataToLocalStorage(item){
 
 function retrieveDataFromLocalStorage(){
     let dataFromLocalStorage;
+    
     if(localStorage.getItem('items') === null){
         dataFromLocalStorage =[]
     }else{ 
 
         dataFromLocalStorage = JSON.parse(localStorage.getItem('items'))
 
-        dataFromLocalStorage.forEach((item, index)=>{
+        return dataFromLocalStorage;
 
-            // clone template.
-
-             const templateClone = template.content.cloneNode(true);
-
-             
-
-             templateClone.querySelector('.logo').src = item.logo;
-             templateClone.querySelector('h2').textContent = item.name;
-             templateClone.querySelector('p').textContent = item.description;
-             const checkbox = templateClone.querySelector('input[type="checkbox"]');
-             const label = templateClone.querySelector('label');
-             const removeBtn = templateClone.querySelector('button')
-
-             const uniqueId = `toggle-${index}`;
-
-            checkbox.id = uniqueId;
-            label.setAttribute('for', uniqueId);
-            checkbox.checked = item.isActive;
-
-            // event handler for updating active status of extension
-            checkbox.addEventListener('change', ()=>{
-                dataFromLocalStorage = JSON.parse(localStorage.getItem("items"))
-
-                dataFromLocalStorage[index].isActive = checkbox.checked
-
-                localStorage.setItem("items", JSON.stringify(dataFromLocalStorage))
-            } )
-
-            // function to remove cards
-            removeBtn.addEventListener('click', function removeCard(){
-
-                console.removeBtn
-
-                cardContainer.innerHTML="";
-                
-
-                dataFromLocalStorage = JSON.parse(localStorage.getItem("items"))
-                dataFromLocalStorage.splice(index,1)
-                localStorage.setItem("items",JSON.stringify(dataFromLocalStorage))
-
-                retrieveDataFromLocalStorage()
-
-            
-
-            })
-
-            //  append clone to container. 
-
-             cardContainer.appendChild(templateClone)
+    }}
+       
 
 
-        })
 
-    }
-}
+        
 
 // starter function.
 function innit(){
     if(localStorage.getItem('items') !== null && JSON.parse(localStorage.getItem('items')).length >0){
         retrieveDataFromLocalStorage()
+        render()
     }else{
 
         //initialize new https request
@@ -131,8 +96,11 @@ function innit(){
         const data = JSON.parse(this.responseText)
         data.forEach(item => {
             sendDataToLocalStorage(item)
-            retrieveDataFromLocalStorage()
+            
+           
         });
+
+         render()
     }
 
     
@@ -183,169 +151,248 @@ function displayFocusBtn(){
         
     }
 
-//displaying active extensions in the dom
 
-function displayActiveExtensionsOnly(){
 
-    cardContainer.innerHTML =""
+    
+    
 
-    retrieveActiveExtensionsDataFromLocalStorage()
+
+
    
-}
-
-// Retrieving active extensions data from local storage
-
- function retrieveActiveExtensionsDataFromLocalStorage(){
-    let dataFromLocalStorage;
-    if(localStorage.getItem('items') === null){
-        dataFromLocalStorage =[]
-    }else{ 
-
-        dataFromLocalStorage = JSON.parse(localStorage.getItem('items'))
-
-        dataFromLocalStorage.forEach((item, index)=>{
-
-            if(item.isActive === true){
-
-                 // clone template.
-
-             const templateClone = template.content.cloneNode(true);
-
-             
-
-             templateClone.querySelector('.logo').src = item.logo;
-             templateClone.querySelector('h2').textContent = item.name;
-             templateClone.querySelector('p').textContent = item.description;
-             const checkbox = templateClone.querySelector('input[type="checkbox"]');
-             const label = templateClone.querySelector('label');
-            
-
-             const uniqueId = `toggle-${index}`;
-
-            checkbox.id = uniqueId;
-            label.setAttribute('for', uniqueId);
-            checkbox.checked = item.isActive;
-
-             // event handler for updating active status of extension
-            checkbox.addEventListener('change', ()=>{
-                dataFromLocalStorage = JSON.parse(localStorage.getItem("items"))
-
-                dataFromLocalStorage[index].isActive = checkbox.checked
-
-                localStorage.setItem("items", JSON.stringify(dataFromLocalStorage))
-            } )
 
 
-            //remove card function
+function updateExtensionStatus(checkbox){
+
+    checkbox.addEventListener('change', ()=>{
+
+    data = retrieveDataFromLocalStorage();
+    data.forEach((item)=>{
+        if(item.id === checkbox.id){
+            item.isActive = checkbox.checked;
 
             
+        }
 
-           
+        
+    })
 
-            //  append clone to container. 
+    localStorage.setItem('items', JSON.stringify(data))
 
-             cardContainer.appendChild(templateClone)
+    render()
+
+} )
+}
+
+// remove card
+function removeExtension(uniqueId, removeBtn){
+removeBtn.addEventListener('click', ()=>{
+
+    data = retrieveDataFromLocalStorage();
+
+    const newData = data.filter((item)=>{
+        if(item.id !== uniqueId ){
+
+            return true
+
+        }  else{
+            return false
+        }
+    })
+
+    localStorage.setItem('items', JSON.stringify(newData))
+
+    render()
 
 
+})
 
-            }
+    
+ 
+}
 
-           
+
+function render(){
+
+    const dataFromLocalStorage = retrieveDataFromLocalStorage();
+
+    cardContainer.innerHTML = "";
+
+
+    
+
+    if(currentFilter === 'active'){ 
+
+        const activeData = dataFromLocalStorage.filter((item)=>{
+
+            return item.isActive
+            
         })
 
-    }
-}
+
+        activeData.forEach((item)=>{
 
 
-//displaying inactive extensions in the dom
+            
+        const templateClone = template.content.cloneNode(true);
 
-function displayInactiveExtensionsOnly(){
+        const card = templateClone.querySelector('.item-card')
+        
+        card.dataset.id = item.id
 
-    cardContainer.innerHTML =""
+        const logo = templateClone.querySelector('.logo') 
 
-    retrieveInactiveExtensionsDataFromLocalStorage()
-   
-}
+        logo.src = item.logo
 
+        templateClone.querySelector('h2').textContent = item.name;
 
+        templateClone.querySelector('p').textContent = item.description;
 
- function retrieveInactiveExtensionsDataFromLocalStorage(){
-    let dataFromLocalStorage;
-    if(localStorage.getItem('items') === null){
-        dataFromLocalStorage =[]
-    }else{ 
+        const checkbox = templateClone.querySelector('input[type ="checkbox"]');
 
-        dataFromLocalStorage = JSON.parse(localStorage.getItem('items'))
+        checkbox.checked = item.isActive
 
-        dataFromLocalStorage.forEach((item, index)=>{
+        const uniqueId = item.id
 
-            if(item.isActive === false){
+        const label = templateClone.querySelector('label');
 
-                 // clone template.
+        checkbox.id = uniqueId;
 
-             const templateClone = template.content.cloneNode(true);
+        label.setAttribute('for', uniqueId)
 
-             
-
-             templateClone.querySelector('.logo').src = item.logo;
-             templateClone.querySelector('h2').textContent = item.name;
-             templateClone.querySelector('p').textContent = item.description;
-             const checkbox = templateClone.querySelector('input[type="checkbox"]');
-             const label = templateClone.querySelector('label');
-
-             const uniqueId = `toggle-${index}`;
-
-            checkbox.id = uniqueId;
-            label.setAttribute('for', uniqueId);
-            checkbox.checked = item.isActive;
-
-            checkbox.addEventListener('change', ()=>{
-                dataFromLocalStorage = JSON.parse(localStorage.getItem("items"))
-
-                dataFromLocalStorage[index].isActive = checkbox.checked
-
-                localStorage.setItem("items", JSON.stringify(dataFromLocalStorage))
-            } )
-
-            //  append clone to container. 
-
-             cardContainer.appendChild(templateClone)
+        const removeBtn = templateClone.querySelector('button')
 
 
+        updateExtensionStatus( checkbox);
 
-            }
+        removeExtension(uniqueId, removeBtn)
 
-           
+
+        cardContainer.appendChild(templateClone);
+
+            
+
+            
+
         })
 
+    } else if( currentFilter === 'inactive'){
+
+        const inActiveData = dataFromLocalStorage.filter((item)=>{
+            return !item.isActive;
+        })
+
+        inActiveData.forEach((item)=>{
+
+
+        const templateClone = template.content.cloneNode(true);   
+            
+        const card = templateClone.querySelector('.item-card')
+        
+        card.dataset.id = item.id
+
+        const logo = templateClone.querySelector('.logo') 
+
+        logo.src = item.logo
+
+        templateClone.querySelector('h2').textContent = item.name;
+
+        templateClone.querySelector('p').textContent = item.description;
+
+        const checkbox = templateClone.querySelector('input[type ="checkbox"]');
+
+        checkbox.checked = item.isActive
+
+        const uniqueId = item.id
+
+        const label = templateClone.querySelector('label');
+
+        checkbox.id = uniqueId;
+
+        label.setAttribute('for', uniqueId)
+
+        const removeBtn = templateClone.querySelector('button')
+
+
+        updateExtensionStatus( checkbox);
+
+        removeExtension(uniqueId, removeBtn)
+
+
+        cardContainer.appendChild(templateClone);
+
+
+
+        })
+    } else{
+
+        
+
+
+        dataFromLocalStorage.forEach((item)=>{
+
+        const templateClone = template.content.cloneNode(true);
+
+        const card = templateClone.querySelector('.item-card')
+        
+        card.dataset.id = item.id
+
+       const logo = templateClone.querySelector('.logo') 
+
+        logo.src = item.logo
+
+        templateClone.querySelector('h2').textContent = item.name;
+
+        templateClone.querySelector('p').textContent = item.description;
+
+        const checkbox = templateClone.querySelector('input[type ="checkbox"]');
+
+        checkbox.checked = item.isActive
+
+        const uniqueId = item.id
+
+        const label = templateClone.querySelector('label');
+
+        checkbox.id = uniqueId;
+
+        label.setAttribute('for', uniqueId)
+
+        const removeBtn = templateClone.querySelector('button')
+
+
+        updateExtensionStatus( checkbox);
+
+        removeExtension(uniqueId, removeBtn)
+
+
+        cardContainer.appendChild(templateClone);
+
+
+
+
+})
+
     }
+
+
+
+    
+
 }
 
+filterInactive.addEventListener('click', ()=>{
+     currentFilter = 'inactive';
 
-function displayAllExtensions(){
-    cardContainer.innerHTML=""
+    render()
+} )
 
-    retrieveDataFromLocalStorage()
-}
+filterActive.addEventListener('click', ()=>{
+     currentFilter = 'active';
 
-
-
-
-// checkbox.addEventListener("change", () => {
-//     const data = JSON.parse(localStorage.getItem("items"))
-
-//     data[index].isActive = checkbox.checked   // true or false
-
-//     localStorage.setItem("items", JSON.stringify(data))
-// })
+    render()})
 
 
 
-filterActive.addEventListener('click', displayActiveExtensionsOnly)
-
-filterInactive.addEventListener('click', displayInactiveExtensionsOnly)
-
-showAll.addEventListener('click', displayAllExtensions )
-
-
-
+showAll.addEventListener('click', ()=>{
+    currentFilter = 'all'
+    render()
+})
